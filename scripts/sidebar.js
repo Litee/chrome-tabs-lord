343c08f97b6be887c175d97136fac44351cde00a
Andrey Lipatkin <lipatkin@gmail.com>
2016-04-16 00:30:35 +0300

    New windows are added, closed windows are removed

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,48 +1,67 @@
 $(document).ready(onReady);
 
 function onReady() {
-	console.log("Sidebar view loaded! Reading information about existing windows...");
+  console.log('Sidebar view loaded! Reading information about existing windows...');
 
-	var jsTree = $('#tree-root').jstree({
-		'core' : {
-			'check_callback': true,
-			'themes' : {
-	      		"dots" : false
-	    	}
-		},
-	});
+  var jsTree = $('#tree-root').jstree({
+    'core': {
+      'check_callback': true,
+      'themes': {
+        'dots': false
+      }
+    },
+  });
 
-	jsTree.on("select_node.jstree",
-        function(evt, data){
-        	var nodeMeta = 	data.node.original;
-        	if (nodeMeta.tabId) {
-				chrome.windows.update(nodeMeta.parentWindowId, {focused:true}, function() {
-					chrome.tabs.update(nodeMeta.tabId, {active:true});
-				});
-        	}
+  var tree = $('#tree-root').jstree(true);
+
+  jsTree.on('select_node.jstree',
+        function(evt, data) {
+          var nodeMeta = data.node.original;
+          if (nodeMeta.tabId) {
+            chrome.windows.update(nodeMeta.parentWindowId, {focused: true}, function() {
+              chrome.tabs.update(nodeMeta.tabId, {active: true});
+            });
+          }
         }
-	);
+    );
 
-	chrome.windows.getAll({populate:true, windowTypes:["normal"]}, function(windowsArr) {
-		windowsArr.forEach(function(window) {
-			console.log("Populating window " + window.id);
-			var windowNodeId = $('#tree-root').jstree("create_node", null, {"text": "Window", "windowId": window.id, "state": {"opened": true}});
-			window.tabs.forEach(function(tab) {
-				console.log("Populating tab " + tab.id);
-				var windowNode = $('#tree-root').jstree("create_node", windowNodeId, {"text": tab.title, "tabId": tab.id, "parentWindowId": tab.windowId, "icon": tab.favIconUrl});
-			});
-		});
-	});
-	chrome.windows.onCreated.addListener(onWindowCreated);
-	chrome.windows.onRemoved.addListener(onWindowRemoved);
+  chrome.windows.getAll({populate: true, windowTypes: ['normal']}, function(windowsArr) {
+    windowsArr.forEach(function(window) {
+      console.log('Populating window ' + window.id);
+      var windowNodeId = tree.create_node(null, {
+        'id': 'window-' + window.id,
+        'text': 'Window',
+        'windowId': window.id,
+        'state': {'opened': true}
+      });
+      window.tabs.forEach(function(tab) {
+        console.log('Populating tab ' + tab.id);
+        var tabNode = tree.create_node(windowNodeId, {
+          'id': 'tab-' + tab.id,
+          'text': tab.title,
+          'tabId': tab.id,
+          'parentWindowId': tab.windowId,
+          'icon': tab.favIconUrl
+        });
+      });
+    });
+  });
+  chrome.windows.onCreated.addListener(onWindowCreated);
+  chrome.windows.onRemoved.addListener(onWindowRemoved);
 
-	function onWindowCreated(window) {
-		var windowNode = jsTree.create_node(null, {"text": "Window", "windowId": window.id});
-	}
+  function onWindowCreated(window) {
+    var windowNodeId = tree.create_node(null, {
+      'id': 'window-' + window.id,
+      'text': 'Window',
+      'windowId': window.id,
+      'state': {'opened': true}
+    });
+  }
 
-	function onWindowRemoved(window) {
-		$("li.window-panel").filter(function(w) { return w.tabsMasterWindowId === window.id}).remove();
-	}
+  function onWindowRemoved(windowId) {
+    console.log('Removing window: ' + windowId);
+    tree.delete_node('window-' + windowId);
+  }
 
-	console.log("Existing windows parsed!");
+  console.log('Existing windows parsed!');
 }
