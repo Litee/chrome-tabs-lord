b348ffae5fb86778a2e4174174d910eda18d519d
Andrey Lipatkin <lipatkin@gmail.com>
2016-04-16 01:02:54 +0300

    * New tabs added, removed * Tab title/icon changes are processed

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -28,29 +28,22 @@ function onReady() {
   chrome.windows.getAll({populate: true, windowTypes: ['normal']}, function(windowsArr) {
     windowsArr.forEach(function(window) {
       console.log('Populating window ' + window.id);
-      var windowNodeId = tree.create_node(null, {
-        'id': 'window-' + window.id,
-        'text': 'Window',
-        'windowId': window.id,
-        'state': {'opened': true}
-      });
+      onWindowCreated(window);
       window.tabs.forEach(function(tab) {
         console.log('Populating tab ' + tab.id);
-        var tabNode = tree.create_node(windowNodeId, {
-          'id': 'tab-' + tab.id,
-          'text': tab.title,
-          'tabId': tab.id,
-          'parentWindowId': tab.windowId,
-          'icon': tab.favIconUrl
-        });
+        onTabCreated(tab);
       });
     });
   });
   chrome.windows.onCreated.addListener(onWindowCreated);
   chrome.windows.onRemoved.addListener(onWindowRemoved);
+  chrome.tabs.onCreated.addListener(onTabCreated);
+  chrome.tabs.onRemoved.addListener(onTabRemoved);
+  chrome.tabs.onUpdated.addListener(onTabUpdated);
 
   function onWindowCreated(window) {
-    var windowNodeId = tree.create_node(null, {
+    console.log('Window created', window);
+    return tree.create_node(null, {
       'id': 'window-' + window.id,
       'text': 'Window',
       'windowId': window.id,
@@ -59,8 +52,33 @@ function onReady() {
   }
 
   function onWindowRemoved(windowId) {
-    console.log('Removing window: ' + windowId);
+    console.log('Window removed', windowId);
     tree.delete_node('window-' + windowId);
+  }
+
+  function onTabCreated(tab) {
+    console.log('Tab created', tab);
+    tree.create_node('window-' + tab.windowId, {
+      'id': 'tab-' + tab.id,
+      'text': tab.title,
+      'tabId': tab.id,
+      'parentWindowId': tab.windowId,
+      'icon': tab.favIconUrl
+    });
+  }
+
+  function onTabRemoved(tabId, removeInfo) {
+    console.log('Tab removed', tabId, removeInfo);
+    tree.delete_node('tab-' + tabId);
+  }
+
+  function onTabUpdated(tabId, changeInfo) {
+    console.log('Tab updated', tabId, changeInfo);
+    // TODO rethink - could be too much overhead
+    chrome.tabs.get(tabId, function(tab) {
+      tree.set_text('tab-' + tabId, tab.title);
+      tree.set_icon('tab-' + tabId, tab.favIconUrl);
+    });
   }
 
   console.log('Existing windows parsed!');
