b7f4be895159111c4b7cef88c177b304c13a9737
Andrey Lipatkin <lipatkin@gmail.com>
2016-04-17 11:59:35 +0300

    * Tabs tree is updated when tab is moved (some flaws are not fixed yet) * Tabs tree is updated when tab is detached into new window * Setting empty icon for some URLs to avoid errors in console

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -16,16 +16,28 @@ function onReady() {
   var tree = $('#tree-root').jstree(true);
 
   jsTree.on('select_node.jstree',
-        function(evt, data) {
-          var nodeMeta = data.node.original;
-          if (nodeMeta.tabId) {
-            chrome.windows.update(nodeMeta.parentWindowId, {focused: true}, function() {
-              chrome.tabs.update(nodeMeta.tabId, {active: true});
-            });
-          }
-        }
-    );
+    function(evt, data) {
+      var nodeMeta = data.node.original;
+      if (nodeMeta.tabId) {
+        chrome.windows.update(nodeMeta.parentWindowId, {focused: true}, function() {
+          chrome.tabs.update(nodeMeta.tabId, {active: true});
+        });
+      }
+    }
+  );
 
+  /*jsTree.on('create_node.jstree', function(e, data) {
+    var tabId = data.node.original.tabId;
+    if (tabId) {
+      var nodeElement = $('li#' + data.node.id).find('a');
+      nodeElement.dblclick(function() {
+        alert('Clicked ' + data.node.id);
+      });
+      var closeIcon = $('<span>').css('background-image', 'url("/images/close.png")').click(function() {
+        chrome.tabs.remove(tabId);
+      }).append('<p>[X]</p>').appendTo(nodeElement);
+    }
+  });*/
   chrome.windows.getAll({populate: true, windowTypes: ['normal']}, function(windowsArr) {
     windowsArr.forEach(function(window) {
       console.log('Populating window', window);
@@ -41,6 +53,8 @@ function onReady() {
   chrome.tabs.onCreated.addListener(onTabCreated);
   chrome.tabs.onRemoved.addListener(onTabRemoved);
   chrome.tabs.onUpdated.addListener(onTabUpdated);
+  chrome.tabs.onMoved.addListener(onTabMoved);
+  chrome.tabs.onAttached.addListener(onTabAttached);
 
   function onWindowCreated(window) {
     console.log('Window created', window);
@@ -57,10 +71,6 @@ function onReady() {
     tree.delete_node('window-' + windowId);
   }
 
-  function onWindowFocusChanged(windowId) {
-
-  }
-
   function onTabCreated(tab) {
     console.log('Tab created', tab);
     tree.create_node('window-' + tab.windowId, {
@@ -68,7 +78,7 @@ function onReady() {
       'text': tab.title,
       'tabId': tab.id,
       'parentWindowId': tab.windowId,
-      'icon': tab.favIconUrl
+      'icon': correctFavIconUrl(tab.favIconUrl)
     });
   }
 
@@ -82,8 +92,25 @@ function onReady() {
     // TODO rethink - could be too much overhead
     chrome.tabs.get(tabId, function(tab) {
       tree.set_text('tab-' + tabId, tab.title);
-      tree.set_icon('tab-' + tabId, tab.favIconUrl);
+      tree.set_icon('tab-' + tabId, correctFavIconUrl(tab.favIconUrl));
     });
+  }
+
+  function correctFavIconUrl(iconUrl) {
+    if (iconUrl && iconUrl.startsWith('chrome://theme/')) {
+      return undefined;
+    }
+    return iconUrl;
+  }
+
+  function onTabMoved(tabId, moveInfo) {
+    console.log('Tab moved', tabId, moveInfo);
+    tree.move_node('tab-' + tabId, 'window-' + moveInfo.windowId, moveInfo.toIndex);
+  }
+
+  function onTabAttached(tabId, attachInfo) {
+    console.log('Tab attached', tabId, attachInfo);
+    tree.move_node('tab-' + tabId, 'window-' + attachInfo.newWindowId, attachInfo.newPosition);
   }
 
   console.log('Existing windows parsed!');
