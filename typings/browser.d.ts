cf86d04d31c08c81ff28e4a5244f075b44ce91d1
Andrey Lipatkin <lipatkin@gmail.com>
2016-05-15 10:43:58 +0300

    Switched from custom jquery-visible definition to DTS one

--- typings/browser.d.ts
+++ typings/browser.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="browser/ambient/chrome-app/index.d.ts" />
 /// <reference path="browser/ambient/chrome/index.d.ts" />
+/// <reference path="browser/ambient/df-visible/index.d.ts" />
 /// <reference path="browser/ambient/filesystem/index.d.ts" />
 /// <reference path="browser/ambient/filewriter/index.d.ts" />
 /// <reference path="browser/ambient/jquery/index.d.ts" />
--- typings/main.d.ts
+++ typings/main.d.ts
@@ -1,5 +1,6 @@
 /// <reference path="main/ambient/chrome-app/index.d.ts" />
 /// <reference path="main/ambient/chrome/index.d.ts" />
+/// <reference path="main/ambient/df-visible/index.d.ts" />
 /// <reference path="main/ambient/filesystem/index.d.ts" />
 /// <reference path="main/ambient/filewriter/index.d.ts" />
 /// <reference path="main/ambient/jquery/index.d.ts" />
